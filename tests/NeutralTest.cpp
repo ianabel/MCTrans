c91df1388d902d0248a69db341c7665169ff902a
Ian Abel <iga@physics.org>
2022-06-13 16:55:10 -0400

    make examples doesn't work, remove, manually update examples on release

---
+++ tests/NeutralTest.cpp
@@ -0,0 +1,20 @@
+
+#include "AtomicPhysics.hpp"
+#include "MirrorPlasma.hpp"
+
+int main( int argc, char** argv )
+{
+	std::map<std::string, double> parameterMap {
+		{ "CentralCellField", 1.0 },
+		{ "Throatfield", 10.0 },
+		{ "PlasmaRadiusMin", 0.2 },
+		{ "PlasmaRadiusMax", 0.6 }
+	};
+	auto pVacConfig = std::make_shared<MirrorPlasma::VacuumMirrorConfiguration>( parameterMap,FuelName,false,tribool::fal,tribool::fal,false,false,false,"","" );
+	auto pReferencePlasmaState = std::make_shared<MirrorPlasma>(pVacuumConfig, parameterMap, VoltageTrace);
+
+	std::cout << protonImpactIonizationCrossSection( 1.0 ) << std::endl;
+
+	return 0;
+
+}
